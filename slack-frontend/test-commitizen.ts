93140e401073fd7b4010083a7d1c031fc20c5aeb
clm1010 <limin_chu@163.com>
2025-12-31 10:15:01 +0800

    chore: 清理测试文件

--- slack-frontend/test-commitizen.ts
+++
@@ -1,4 +0,0 @@
-// 测试 commitizen 交互式提交
-export const testCommitizen = () => {
-  console.log('Testing commitizen workflow')
-}
--- slack-frontend/test-hook.ts
+++
@@ -1,4 +0,0 @@
-// 测试 husky + lint-staged
-const obj = { name: 'test', value: 123 }
-
-export default obj
